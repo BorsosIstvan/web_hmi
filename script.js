b9adda3722b85617ffc9343ba9f5b263939cd5b1
BorsosIstvan <bistvanus@gmail.com>
2025-05-25 18:15:19 +0200

    raspberry

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const client = mqtt.connect('ws://10.100.103.16:9001'); // Pas eventueel IP/hostname aan
+const client = mqtt.connect('ws://raspberrypi.local:9001'); // Pas eventueel IP/hostname aan
 
 client.on('connect', () => {
   console.log("MQTT verbonden");
