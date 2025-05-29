6bdeff3615298a9ec953526de7ed957dcff420f1
BorsosIstvan <bistvanus@gmail.com>
2025-05-29 13:01:29 +0200

    ook testen

--- php/load_message.php
+++ php/load_message.php
@@ -1,7 +1,7 @@
 <?php
 // Bestand: load_messages.php
 
-$file = '/messages/berichten.json';
+$file = 'messages/berichten.json';
 
 if (file_exists($file)) {
     echo file_get_contents($file);
--- php/testwrite.php
+++ php/testwrite.php
@@ -1,5 +1,5 @@
 <?php
-$file = '/messages/berichten.json';
+$file = 'tekst.json';
 $data = array("test" => "schrijven lukt");
 file_put_contents($file, json_encode($data, JSON_PRETTY_PRINT));
 echo "Schrijven voltooid";
